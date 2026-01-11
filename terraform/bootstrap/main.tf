988965e0f6d1017c80ee67f3a6b419be44feb733
makthuddinshaik87 <makthuddin.shaik87@gmail.com>
2026-01-11 14:11:22 +0530

    Update main.tf
    updated

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -1,19 +1,30 @@
+############################################
+# Provider
+############################################
 provider "google" {
   project = var.project_id
   region  = var.region
 }
 
-# Enable required APIs
-resource "google_project_service" "apis" {
+############################################
+# Enable Required GCP APIs
+############################################
+resource "google_project_service" "required_apis" {
   for_each = toset([
     "compute.googleapis.com",
     "iam.googleapis.com",
-    "storage.googleapis.com"
+    "storage.googleapis.com",
+    "sqladmin.googleapis.com",        
+    "serviceusage.googleapis.com"
   ])
+
   service = each.key
+  disable_on_destroy = false
 }
 
-# Terraform state bucket
+############################################
+# GCS Bucket for Terraform Remote State
+############################################
 resource "google_storage_bucket" "tf_state" {
   name          = var.tf_state_bucket
   location      = var.region
@@ -22,25 +33,33 @@ resource "google_storage_bucket" "tf_state" {
   uniform_bucket_level_access = true
 }
 
-# GitHub Actions service account
+############################################
+# GitHub Actions Service Account
+############################################
 resource "google_service_account" "github" {
   account_id   = "github-actions-sa"
-  display_name = "GitHub Actions Terraform SA"
+  display_name = "GitHub Actions Terraform Service Account"
 }
 
-# IAM bindings
-resource "google_project_iam_member" "sa_roles" {
+############################################
+# IAM Roles for GitHub Actions Service Account
+############################################
+resource "google_project_iam_member" "github_sa_roles" {
   for_each = toset([
     "roles/compute.admin",
     "roles/storage.admin",
-    "roles/iam.serviceAccountUser"
+    "roles/iam.serviceAccountUser",
+    "roles/cloudsql.admin",                
+    "roles/serviceusage.serviceUsageAdmin" 
   ])
 
   role   = each.key
   member = "serviceAccount:${google_service_account.github.email}"
 }
 
-# Service account key
+############################################
+# Service Account Key (Used in GitHub Secrets)
+############################################
 resource "google_service_account_key" "github_key" {
   service_account_id = google_service_account.github.name
 }
