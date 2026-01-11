d0d4757d08ec8db4b7380595a44eb88ae4374a85
makthuddinshaik87 <makthuddin.shaik87@gmail.com>
2026-01-11 13:48:15 +0530

    Create outputs.tf

---
+++ terraform/part1-db/outputs.tf
@@ -0,0 +1,15 @@
+output "db_ip" {
+  value = google_sql_database_instance.postgres.public_ip_address
+}
+ 
+output "db_port" {
+  value = 5432
+}
+ 
+output "db_name" {
+  value = google_sql_database.bindplane.name
+}
+ 
+output "db_user" {
+  value = google_sql_user.bindplane.name
+}
