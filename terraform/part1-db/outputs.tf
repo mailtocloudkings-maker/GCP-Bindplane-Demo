1a7f852169839932d85ef1ec586cf23b2e4c26a4
makthuddinshaik87 <makthuddin.shaik87@gmail.com>
2026-01-11 19:32:26 +0530

    Update outputs.tf

--- terraform/part1-db/outputs.tf
+++ terraform/part1-db/outputs.tf
@@ -1,15 +1,16 @@
-output "db_ip" {
-  value = google_sql_database_instance.postgres.public_ip_address
+output "db_host" {
+  description = "Cloud SQL private/public IP"
+  value       = google_sql_database_instance.postgres.ip_address[0].ip_address
 }
- 
+
 output "db_port" {
   value = 5432
 }
- 
+
 output "db_name" {
   value = google_sql_database.bindplane.name
 }
- 
+
 output "db_user" {
   value = google_sql_user.bindplane.name
 }
