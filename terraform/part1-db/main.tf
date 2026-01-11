b71c903294ad3968f32a44a94525886d0be40357
makthuddinshaik87 <makthuddin.shaik87@gmail.com>
2026-01-11 13:35:13 +0530

    Update main.tf

--- terraform/part1-db/main.tf
+++ terraform/part1-db/main.tf
@@ -2,47 +2,32 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
-data "terraform_remote_state" "db" {
-  backend = "gcs"
-  config = {
-    bucket = "bindplane-tf-state-demo"
-    prefix = "bindplane/part1-db"
-  }
-}
-
-resource "google_compute_instance" "control_plane" {
-  name         = "bp-control-plane"
+ 
+resource "google_compute_instance" "postgres" {
+  name         = "bp-postgres"
   machine_type = "e2-micro"
   zone         = "us-central1-a"
-
+ 
   boot_disk {
     initialize_params {
       image = "debian-12"
-      size  = 20
+      size  = 10
     }
   }
-
+ 
   network_interface {
     network = "default"
-    access_config {}
   }
-
+ 
   metadata_startup_script = <<EOF
 #!/bin/bash
-curl -sSL https://observiq.com/install-bindplane.sh | bash
-
-bindplane setup \
-  --db-host ${data.terraform_remote_state.db.outputs.db_ip} \
-  --db-port 5432 \
-  --db-user bindplane \
-  --db-password ${var.db_password} \
-  --admin-password ${var.admin_password}
-
-bindplane api-keys create demo-agent --json > /tmp/api.json
+apt update
+apt install -y postgresql
+sudo -u postgres psql -c "CREATE USER bindplane WITH PASSWORD '${var.db_password}';"
+sudo -u postgres psql -c "CREATE DATABASE bindplane OWNER bindplane;"
 EOF
 }
-
-output "control_plane_ip" {
-  value = google_compute_instance.control_plane.network_interface[0].access_config[0].nat_ip
+ 
+output "db_ip" {
+  value = google_compute_instance.postgres.network_interface[0].network_ip
 }
